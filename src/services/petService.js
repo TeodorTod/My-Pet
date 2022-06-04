ee505d38b79dff476483237b2833c75d1bc7ad04
Artos Web <teodor.at.tod@gmail.com>
2022-06-05 00:02:54 +0300

    add pet

--- src/services/petService.js
+++ src/services/petService.js
@@ -9,3 +9,17 @@ export const getAll = async () => {
 
     return result; 
 };
+
+export const create = async (petData) => {
+    let res = await fetch(`${baseUrl}/pets`, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(petData)
+    });
+
+    let data = await res.json();
+    
+    return data;
+}
